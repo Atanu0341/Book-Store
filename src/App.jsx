f681625e41b8a3611316690eb940284eb6e95371
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-19 12:20:23 +0530

    Refactor App component to include React import and remove unnecessary div tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import React from "react";
+
 function App() {
   return (
     <>
-    <div className="bg-red-200">
-
-      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
-    </div>
+      <div className="bg-red-200">
+        <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+      </div>
     </>
   );
 }
