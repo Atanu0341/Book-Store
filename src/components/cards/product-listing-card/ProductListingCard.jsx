904681bb8ecc79b9ea1057565ecefd50e6a15eb6
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 22:02:42 +0530

    Refactor ProductListingCard component to use dynamic book data

--- src/components/cards/product-listing-card/ProductListingCard.jsx
+++ src/components/cards/product-listing-card/ProductListingCard.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import ProductImage from "../../../assets/books-images/harry-potter.jpg";
 
-const ProductListingCard = () => {
+const ProductListingCard = ({bookData}) => {
   return (
-    <div className="border border-blue-200 w-fit p-8 rounded-lg">
-      <div className="flex justify-center items-center">
-        <img src={ProductImage} alt="product-listing-image" className="w-2/3" />
+    <div className="border border-blue-200 w-full p-8 rounded-lg">
+      <div className="flex justify-center items-center w-full h-80">
+        <img src={bookData.book_url} alt="product-listing-image" className="w-full h-full object-contain" />
       </div>
 
       <div className="pt-8 ">
         <h3 className="text-xl font-bold">
-          Harry Potter and the Philosopher's Stone
+          {bookData.book_name}
         </h3>
-        <p className="py-2 text-sm">J. K. Rowling</p>
-        <p className="text-xl font-bold text-[#fb995d]">&#8377; 353</p>
+        <p className="py-2 text-sm">{bookData.author_name}</p>
+        <p className="text-xl font-bold text-[#fb995d]">&#8377;{bookData.price}</p>
         <a
           href="#"
           className="bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in mt-3 inline-block w-full py-4 text-center text-white"
