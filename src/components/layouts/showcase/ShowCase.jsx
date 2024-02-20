fb81fc5373c43967baeec4d9894dba7bf379aa0b
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 12:54:48 +0530

    Add search input form to showcase layout

--- src/components/layouts/showcase/ShowCase.jsx
+++ src/components/layouts/showcase/ShowCase.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ShowCasebg from "../../../assets/showcase-bg.jpg";
 import Navbar from "../navbar/Navbar";
+import SearchInputForm from "../../forms/searchInputForm/SearchInputForm";
 
 const ShowCase = () => {
   return (
-    <section className="relative h-screen flex items-center justify-center bg-cover" style={{backgroundImage: `url(${ShowCasebg})`}}>
-        <Navbar />
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+    <section className="relative h-screen flex items-center justify-center bg-cover"
+      style={{ backgroundImage: `url(${ShowCasebg})` }}>
+      <Navbar />
+      <div className="absolute inset-0 bg-black opacity-40"></div>
       <div className="text-white text-center z-10">
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
           Best <span className="text-[#fb995d]">Books</span> Available
@@ -14,6 +16,7 @@ const ShowCase = () => {
         <p className="text-lg md:text-xl lg:text-2xl">
           Buy quality books at cheaper price
         </p>
+        <SearchInputForm />
       </div>
     </section>
   );
