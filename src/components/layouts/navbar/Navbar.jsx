97ce2ce2e3dafc08e0dc69636397cc45ef551483
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 16:38:53 +0530

    Refactor Navbar component to support dark theme

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ darkTheme }) => {
   return (
-    <section className='absolute top-0 w-full z-[1] py-4'>
-        <div className='w-11/12 m-auto flex justify-between items-center '>
-            <a href="#" className='text-2xl font-bold text-white'>Book<span className='text-[#fb995d]'>World</span></a>
+    <section className={`absolute top-0 w-full z-[1] py-4 ${darkTheme ? 'bg-[#333] relative' : 'bg-transparent'}`}>
+      <div className="w-11/12 m-auto flex justify-between items-center ">
+        <a href="#" className="text-2xl font-bold text-white">
+          Book<span className="text-[#fb995d]">World</span>
+        </a>
 
-            <nav className=''>
-                <a href="#" className='text-white text-xl pl-5 transition duration-150 ease-in hover:text-[#fb995d]'>Home</a>
-                <a href="#" className='text-white text-xl pl-5 transition duration-150 ease-in hover:text-[#fb995d]'>Books</a> 
-            </nav>
-        </div>
+        <nav className="">
+          <a
+            href="#"
+            className="text-white text-xl pl-5 transition duration-150 ease-in hover:text-[#fb995d]"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-white text-xl pl-5 transition duration-150 ease-in hover:text-[#fb995d]"
+          >
+            Books
+          </a>
+        </nav>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
