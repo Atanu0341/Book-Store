4c6366860504e7974e8082503bb9acf810d163d4
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 22:02:52 +0530

    Refactor product listing to use BookData

--- src/components/layouts/product-listing-all/ProductListingAll.jsx
+++ src/components/layouts/product-listing-all/ProductListingAll.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import ProductListingCard from "../../cards/product-listing-card/ProductListingCard";
+import { BookData } from "../../../util/BookData";
 
 const ProductListingAll = () => {
+  console.log(BookData);
   return (
     <section className="pt-20">
       <div className="w-11/12 m-auto ">
         <div className="flex flex-wrap -mx-2">
-          {[...Array(8)].map((_, index) => (
-            <div
-              key={index}
-              className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 py-8 px-2"
-            >
-              <ProductListingCard />
-            </div>
-          ))}
+          {BookData.map((book) => {
+            return (
+              <div key={book.id} className="w-full sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 py-8 px-2">
+                <ProductListingCard bookData={book}/>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
