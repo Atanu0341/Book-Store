03f3ed05b248e086d664a90a5de55959ec0d9ce6
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 10:55:02 +0530

    Add ShowCase component to HomePage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import ShowCase from '../../components/layouts/showcase/ShowCase'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section>
+        <ShowCase />
+    </section>
   )
 }
 
