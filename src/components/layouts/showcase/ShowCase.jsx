c812eb37312d1152629032717a02480d35f3d036
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 10:54:54 +0530

    Add ShowCase component to display books in the showcase section

---
+++ src/components/layouts/showcase/ShowCase.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ShowCasebg from "../../../assets/showcase-bg.jpg";
+import Navbar from "../navbar/Navbar";
+
+const ShowCase = () => {
+  return (
+    <section className="relative h-screen flex items-center justify-center bg-cover" style={{backgroundImage: `url(${ShowCasebg})`}}>
+        <Navbar />
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="text-white text-center z-10">
+        <h1 className="text-4xl md:text-6xl lg:text-7xl font-bold">
+          Best <span className="text-[#fb995d]">Books</span> Available
+        </h1>
+        <p className="text-lg md:text-xl lg:text-2xl">
+          Buy quality books at cheaper price
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default ShowCase;
