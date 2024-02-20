9f48faec90289f842e2d03356eb60f9e9c3a054c
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 16:39:22 +0530

    Refactor BooksPage component and add new components

--- src/pages/bookspage/BooksPage.jsx
+++ src/pages/bookspage/BooksPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/layouts/navbar/Navbar";
+import Footer from "../../components/layouts/footer/Footer";
+import SearchInputForm from "../../components/forms/searchInputForm/SearchInputForm";
+import ProductListingAll from "../../components/layouts/productlistingall/ProductListingAll";
 
 const BooksPage = () => {
   return (
-    <div>BooksPage</div>
-  )
-}
+    <section>
+      <Navbar darkTheme={true} />
+      <div className="flex flex-col lg:flex-rowjustify-center items-center pt-4">
+        <h2 className="text-center pb-4">
+          Find the <span className="text-[#fb995d]">Books</span> that you want
+        </h2>
+        <SearchInputForm darkTheme={false} />
+      </div>
+      <ProductListingAll />
+      <Footer />
+    </section>
+  );
+};
 
-export default BooksPage
+export default BooksPage;
