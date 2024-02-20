bec857df8434a47235841866a8ab1449e6d21640
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-20 14:06:23 +0530

    Add ProductListing component to HomePage

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ShowCase from '../../components/layouts/showcase/ShowCase'
+import ProductListing from '../../components/layouts/productlisting/ProductListing'
 
 const HomePage = () => {
   return (
     <section>
         <ShowCase />
+        <ProductListing />
     </section>
   )
 }
