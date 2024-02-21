69c44a761d7e68095207dc1179b0e0560d8c5ad9
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 10:48:17 +0530

    Refactor DetailsSection component and add dynamic book data

--- src/components/layouts/details-section/DetailsSection.jsx
+++ src/components/layouts/details-section/DetailsSection.jsx
@@ -1,28 +1,60 @@
-import React from 'react'
-import BookDetailImg from '../../../assets/books-images/sherlock-holmes.jpg'
+import React, { useEffect, useState } from "react";
+import BookDetailImg from "../../../assets/books-images/sherlock-holmes.jpg";
+import { useParams } from "react-router-dom";
+import { BookData } from "../../../util/BookData";
 
 const DetailsSection = () => {
-  return (
-    <section className='py-20'>
-      <div className='w-11/12 m-auto'>
-        <div className='flex flex-col justify-start md:flex-row items-center gap-8 lg:items-start'>
-          <div className='w-2/4 md:w-full lg:w-4/5 xl:w-2/5 flex justify-center'>
-            <img src={BookDetailImg} alt="book" className='h-4/5 md: xl:h-2/4 border bottom-1 border-slate-400'  />
-          </div>
-          <div className='flex flex-col gap-4'>
-            <h2 className='text-4xl font-bold'>The Case-Book of Sherlock Holmes Book </h2>
-            <p className='text-[#fb995d] font-semibold text-xl'>Arthur Conan Doyle</p>
-            <p className='text-base'>The Case-Book of Sherlock Holmes is the final set of twelve (out of a total of fifty-six) Sherlock Holmes short stories by British writer Arthur Conan Doyle first published in the Strand Magazine between October 1921 and April 1927.</p>
-            <p><strong>Language:</strong> English</p>
-            <p><strong>Book Length:</strong> 300</p>
-            <h3 className='text-xl font-bold text-[#ff904c] pt-4'>&#8377;129</h3>
+  const { id } = useParams();
+  const [bookData, setBookData] = useState({});
 
-            <a href="#" className='inline-block py-4 px-8 w-2/5 sm:w-2/4 md:w-2/5 lg:w-[30%] xl:w-1/5 rounded-lg text-white bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in'>🛒 Add To Cart</a>
+  useEffect(() => {
+    let newData = BookData.filter((book) => book.id === parseInt(id));
+    
+    setBookData(newData[0])
+  }, []);
+
+  return (
+    <section className="py-20">
+      <div className="w-11/12 m-auto">
+        <div className="flex flex-col justify-start md:flex-row items-center gap-8 lg:items-start">
+          <div className="w-2/4 md:w-full lg:w-4/5 xl:w-2/5 flex justify-center">
+            <img
+              src={bookData.book_url}
+              alt="book"
+              className="h-4/5 md: xl:h-2/4 border bottom-1 border-slate-400"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-4xl font-bold">
+              {bookData.book_name}
+            </h2>
+            <p className="text-[#fb995d] font-semibold text-xl">
+              {bookData.author_name}
+            </p>
+            <p className="text-base">
+              {bookData.book_description}
+            </p>
+            <p>
+              <strong>Language:</strong> {bookData.language}
+            </p>
+            <p>
+              <strong>Book Length:</strong> {bookData.print_length}
+            </p>
+            <h3 className="text-xl font-bold text-[#ff904c] pt-4">
+              &#8377;{bookData.price}
+            </h3>
+
+            <a
+              href="#"
+              className="inline-block py-4 px-8 w-2/5 sm:w-2/4 md:w-2/5 lg:w-[30%] xl:w-1/5 rounded-lg text-white bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in"
+            >
+              🛒 Add To Cart
+            </a>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default DetailsSection
+export default DetailsSection;
