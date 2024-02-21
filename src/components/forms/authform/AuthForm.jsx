0d33bda1efeb97512cc816a9cea00a9924b69abc
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 13:10:15 +0530

    Add AuthForm component

---
+++ src/components/forms/authform/AuthForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const AuthForm = ({ buttonName }) => {
+  return (
+    <form>
+      <div className="pt-4">
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="w-full p-4 text-base rounded-lg outline-none border-2 border-black"
+        />
+      </div>
+      <div className="pt-4">
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className="w-full p-4 text-base rounded-lg outline-none border-2 border-black"
+        />
+      </div>
+      <div className="pt-4">
+        <input
+          type="submit"
+          value={buttonName}
+          className="inline-block cursor-pointer py-4 px-8 w-2/5 sm:w-2/4 md:w-2/5 lg:w-[30%] xl:w-1/5 rounded-lg text-white bg-slate-800 hover:bg-slate-700 transition duration-150 ease-in"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default AuthForm;
