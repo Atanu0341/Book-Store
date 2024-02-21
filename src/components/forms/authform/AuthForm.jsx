f257e3c0917ae53e6689494d7f1b0e24f144187e
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 16:33:50 +0530

    Add form functionality and user authentication

--- src/components/forms/authform/AuthForm.jsx
+++ src/components/forms/authform/AuthForm.jsx
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import app from "../../../firebase/Firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const AuthForm = ({ buttonName }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const auth = getAuth(app);
+
+    if (buttonName === "Login") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+          userCredentials.user.displayName = username;
+
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
+      {buttonName === "Sign Up" && (
+        <div className="pt-4">
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            className="w-full p-4 text-base rounded-lg outline-none border-2 border-black"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+      )}
       <div className="pt-4">
         <label>Email</label>
         <input
           type="email"
           placeholder="Enter your email"
           className="w-full p-4 text-base rounded-lg outline-none border-2 border-black"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </div>
       <div className="pt-4">
@@ -17,6 +68,8 @@ const AuthForm = ({ buttonName }) => {
           type="password"
           placeholder="Enter your password"
           className="w-full p-4 text-base rounded-lg outline-none border-2 border-black"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div className="pt-4">
