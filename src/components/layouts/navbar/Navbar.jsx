974bd6de6cf9cf43e6cdff31333942d7b3006528
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 13:17:05 +0530

    Update Navbar styling and text color

--- src/components/layouts/navbar/Navbar.jsx
+++ src/components/layouts/navbar/Navbar.jsx
@@ -14,30 +14,30 @@ const Navbar = ({ darkTheme, darkText }) => {
     <section className={`absolute top-0 w-full z-[1] py-4 ${darkTheme ? 'bg-[#333] relative' : 'bg-transparent'}`}>
       <div className="w-11/12 m-auto flex justify-between items-center relative">
         <a href="#" className="text-2xl font-bold text-white">
-          Book<span className="text-[#fb995d]">World</span>
+          Book<span className={`text-[${darkText ? 'black' : 'white'}]`}>World</span>
         </a>
 
         {/* Toggle button with icon for small screens */}
         <div className="lg:hidden">
-          <button onClick={toggleMenu} className="text-white">
+          <button onClick={toggleMenu} className={`text-white`}>
             {menuOpen ? (
-              <FontAwesomeIcon icon={faTimes} />
+              <FontAwesomeIcon icon={faTimes} className={`text-${darkText ? 'black' : 'white'}`} />
             ) : (
-              <FontAwesomeIcon icon={faBars} />
+              <FontAwesomeIcon icon={faBars} className={`text-${darkText ? 'black' : 'white'}`} />
             )}
           </button>
         </div>
 
         {/* Horizontal menu for small screens */}
-        <nav className={`lg:hidden absolute top-16 right-0 ${menuOpen ? 'block' : 'hidden'} bg-slate-800 rounded-lg w-3/12 p-4`}>
-          <Link to="/" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-black' : 'text-white'}`}>
+        <nav className={`lg:hidden absolute top-16 right-0 ${menuOpen ? 'block' : 'hidden'} bg-slate-700 rounded-lg w-3/12 p-4`}>
+          <Link to="/" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-red-100' : 'text-white'}`}>
             Home
           </Link>
-          <Link to="/books" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-black' : 'text-white'}`}>
+          <Link to="/books" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-red-100' : 'text-white'}`}>
             Books
           </Link>
-          <Link to="/login" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-black' : 'text-white'}`}>Login</Link>
-          <Link to="/signup" className={`block text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-black' : 'text-white'}`}>Sign up</Link>
+          <Link to="/login" className={`block mb-3 text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-red-100' : 'text-white'}`}>Login</Link>
+          <Link to="/signup" className={`block text-xl transition duration-150 ease-in hover:text-[#fb995d] ${darkText ? 'text-red-100' : 'text-white'}`}>Sign up</Link>
         </nav>
 
         {/* Regular vertical menu for larger screens */}
