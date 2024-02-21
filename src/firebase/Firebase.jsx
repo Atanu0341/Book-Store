53933318a376cecbc6b59ae03c6cf9c0c1bb178c
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 16:35:17 +0530

    Add Firebase configuration and initialization

---
+++ src/firebase/Firebase.jsx
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAue896X8iW4US8i3RFIKZW8DS6VsPNxJY",
+  authDomain: "vite-bookworld.firebaseapp.com",
+  projectId: "vite-bookworld",
+  storageBucket: "vite-bookworld.appspot.com",
+  messagingSenderId: "121577843819",
+  appId: "1:121577843819:web:e5e78bf2068d7e4e61552e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
