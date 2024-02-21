4c6cefb7a57570f7f570a94841fac511c4856ae7
Atanu Majumder <atanumajumder2004@gmail.com>
2024-02-21 16:33:40 +0530

    Add user authentication and cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useEffect, createContext, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
 import BooksPage from "./pages/books-page/BooksPage";
 import BookDetails from "./pages/book-details-page/BookDetails";
 import SignUp from "./pages/sign-up/SignUp";
 import Login from "./pages/login/Login";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "./firebase/Firebase";
+import CartPage from "./components/layouts/cart-page/CartPage";
+
+export const UserContext = createContext({});
 
 const App = () => {
+  const auth = getAuth(app);
+
+  const [authenticatedUser,  setAuthenticatedUser] = useState(null);
+  
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthenticatedUser(user);
+      } else {
+        setAuthenticatedUser(null);
+      }
+    });
+  }, []);
+
   return (
     <>
       <Router>
-        <div className="">
+        <UserContext.Provider value={authenticatedUser}>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/books" element={<BooksPage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="/book-details/:id" element={<BookDetails />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
           </Routes>
-        </div>
+        </UserContext.Provider>
       </Router>
     </>
   );
